7f148847d17a105ea70e6cfe75322a2d5c359e51
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-09-22 22:53:20 +0530

    Create sum & mul functions.py

---
+++ sum & mul functions.py
@@ -0,0 +1,9 @@
+#functions for addition and multiplication
+def add(a,b):
+    return a+b
+def mul(a,b):
+    return a*b
+m = int(input("Enter a num: "))
+n = int(input("Enter a num: "))
+print("The sum is: ",add(m,n))
+print("The product is: ",mul(m,n))
