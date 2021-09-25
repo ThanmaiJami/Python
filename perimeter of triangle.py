6ba5daf1678d23240c3e1bb453f319d223c75618
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-09-25 19:35:24 +0530

    Update perimeter of triangle.py

--- perimeter of triangle.py
+++ perimeter of triangle.py
@@ -3,12 +3,12 @@ class triangle:
         self.a = a
         self.b = b
         self.c = c
-    def perimeter(a,b,c):
+    def perimeter(self):
         return self.a+self.b+self.c
 def main():
     a = int(input("Enter first side: "))
     b = int(input("Enter second side: "))
     c = int(input("Enter third side: "))
-    obj = triangle()
+    obj = triangle(a,b,c)
     print("The perimeter of the triangle: ",obj.perimeter())
 main()
