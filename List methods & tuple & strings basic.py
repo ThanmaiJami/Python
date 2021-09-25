c4141da6afebc2e95b4d8fe101c84189732e0ee0
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-09-25 22:16:29 +0530

    Update and rename List methods & tuple basic.py to List methods & tuple & strings basic.py

--- List methods & tuple & strings basic.py
+++ List methods & tuple & strings basic.py
@@ -5,6 +5,24 @@ mix_l[1]="Hi"
 print("2.",mix_l)
 mix_t = (1,2,3,4,5)
 print("3.",mix_t)"""
+
+
+list = [23,9,88]
+print(list[:3])
+print(len(list))
+print(type(list))
+
+tuple = (1,8,99,34)
+print(tuple[:4])
+print(len(tuple))
+print(type(tuple))
+
+string = "Python"
+print(string[0:6])
+print(len(string))
+print(type(string))
+
+
 list = [12,78,374,90]
 for i in list[:3]:
     print(i)
