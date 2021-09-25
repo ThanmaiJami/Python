cc1b513a49aa6655da76387826dc778713ee2f20
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-09-25 19:57:02 +0530

    Create import statements.py

---
+++ import statements.py
@@ -0,0 +1,19 @@
+import math
+n = 25
+result = math.sqrt(n)
+print(result)
+print(math.pi)
+
+import math as m
+print(m.pi)
+
+from math import sqrt
+n = sqrt(64)
+print(n)
+
+from math import pi,sin,sqrt
+value = sin(pi/2)
+print(value)
+
+import math
+print(dir(math))
