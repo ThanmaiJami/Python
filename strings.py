979b6d57927ec437d5b8fab2d10d371ac8821a0d
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-10-01 22:19:38 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -21,3 +21,16 @@ print("The length of the text1 is ",res3)
 #print("on" in text1) to check the mentioned string is present in text1 or not
 print("on" in text1)
 print("Adaf"in text2)
+
+
+#string methods
+s = "I like Python"
+res1 = s.lower()#lowercase method
+res2 = s.upper()#uppercase method
+print("lower: ",res1, "upper: ",res2)
+#to find index of "python" substring in s
+res3 = s.find("Python")
+print(res3)
+#replace method
+res4 = s.replace("Python","Java")
+print(res4)
