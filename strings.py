778a17a2446975e62e33ccf1956ae448a33176a7
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-10-01 22:11:05 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,3 +1,23 @@
 string = "He said \"What\'s there\""
 print(string)
 #we use  back slash when these trivial sentences are used as strings...
+
+
+#concatenation
+text1 = "Python"
+text2 = "Programmming"
+res1 = text1 + " " + text2
+print(res1)
+#multiply string
+res2 = text1*4
+print(res2)
+print(text2*5)
+#iterating through string
+for character in res1:
+    print(character)
+#length of string
+res3 = len(text1)
+print("The length of the text1 is ",res3)
+#print("on" in text1) to check the mentioned string is present in text1 or not
+print("on" in text1)
+print("Adaf"in text2)
