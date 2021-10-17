d8f28feac2d3f504f3afbfbe74db67f36652a6d5
ThanmaiJami <70189537+ThanmaiJami@users.noreply.github.com>
2021-10-17 23:48:46 +0530

    Create File handling.py

---
+++ File handling.py
@@ -0,0 +1,81 @@
+#File handling
+#Python program to read file word by word
+with open('GFG.txt','r') as file:
+    # reading each line    
+    for line in file:
+        print(line)
+        print(line.split())
+        
+        # reading each word        
+        for word in line.split():
+            # displaying the words           
+            print(word) 
+            
+#output:
+"""Hello! Welocme to file handling in python...
+['Hello!', 'Welocme', 'to', 'file', 'handling', 'in', 'python...']
+Hello!
+Welocme
+to
+file
+handling
+in
+python...
+>>> """
+
+#Python program to read character by character from a file
+file = open('GFG.txt', 'r')
+while 1:
+    # read by character
+    char = file.read(1)         
+    if not char:
+        break
+    print(char)
+file.close()
+
+#output:
+"""H
+e
+l
+l
+o
+!
+ 
+W
+e
+l
+o
+c
+m
+e
+ 
+t
+o
+ 
+f
+i
+l
+e
+ 
+h
+a
+n
+d
+l
+i
+n
+g
+ 
+i
+n
+ 
+p
+y
+t
+h
+o
+n
+.
+.
+.
+>>> """
